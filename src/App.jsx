d2bfebc4f21c51749c4544d31c4e22155448c37f
I-Sicarius-I <game7profile@gmail.com>
2026-05-08 15:38:47 +0200

    loading and displaying movie titles from tmdb api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,68 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './components/Search'
+import Spinner from './components/Spinner'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS = {
+    method: "GET",
+    headers : {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+    }
+}
 
 const App = () => {
 
     const [searchTerm, setSearchTerm] = useState("")
+    const [movieList, setMovieList] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            setIsLoading(true)
+            setErrorMessage('')
+
+            try{
+
+                //try how Spinner looks
+                // const sleep = ms => new Promise(r => setTimeout(r, ms))
+                // await sleep(1000)
+
+                const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+
+                const response = await fetch(endpoint, API_OPTIONS);
+                
+                if(!response.ok){
+                    throw new Error('Failed to fetch movies.')
+                }
+
+                const data = await response.json();
+
+                if(data.success === false){
+                    setErrorMessage(data.error || 'Failed to fetch movies');
+                    setMovieList([])
+                    return;
+                }
+
+                setMovieList(data.results || [])
+            }
+            catch(e)
+            {
+                console.error(`Error fetching movies: ${e}`);
+                setErrorMessage('Error fetching movies. Please try again later.')
+            }
+            finally
+            {
+                setIsLoading(false)
+            }
+        };
+
+        fetchMovies();
+    }, [])
 
     return (
         <main>
@@ -17,6 +76,26 @@ const App = () => {
                         Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle
                     </h1>
                 </header>
+
+                <section className='all-movies'>
+
+                    <h2 className='mt-[40px]'>
+                        All Movies
+                    </h2>
+
+                    {isLoading ? (
+                        <Spinner/>
+                    ) : errorMessage ? (
+                        <p className="text-red-500">{errorMessage}</p>
+                    ) : (
+                        <ul>
+                            {movieList.map((movie) => (
+                                <p key={movie.id} className='text-white'>{movie.title}</p>
+                            ))}
+                        </ul>
+                    )}
+                </section>
+
 
                 <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
             </div>
