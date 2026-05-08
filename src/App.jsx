28e16d95cd5e2326a71bb2ea20fb13bce776bb84
I-Sicarius-I <game7profile@gmail.com>
2026-05-08 13:29:57 +0200

    States, Hooks, useEffect, react snippets

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React from 'react'
 
-//Class component
-// class ClassComponent extends React.Component
-// {
-//   render(){
-//     return <h2>Class Component</h2>
-//   }
-// }
-const Card = ({title}) => {
-  return(
-    <div className='card'>
-      <h2>{title}</h2>
-    </div>
-  )
-}
 const App = () => {
   return (
-    <div className="card-container">
-      <Card title="Star Wars" rating={5} actors={[{name: 'Actor'}]}/>
-      <Card title="Avatar"/>
-      <Card title="The Lion King"/>
+    <div>
+      
     </div>
   )
 }
